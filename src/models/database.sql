53b9ddcd06f5dd61e68ed1c11a798e366833f567
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-23 13:20:27 +0800

    feat: setup postgres connection and database

---
+++ src/models/database.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE shots_ahmadihsan;
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  alt_text VARCHAR(50) NOT NULL,
+  caption TEXT,
+  location TEXT,
+  aws_s3_path TEXT NOT NULL,
+  cloudinary_path TEXT NOT NULL,
+  taken_at timestamp with time zone default NOT NULL,
+  created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE stories (
+  id SERIAL PRIMARY KEY,
+  alt_text VARCHAR(50) NOT NULL,
+  caption TEXT,
+  location TEXT,
+  aws_s3_path TEXT NOT NULL,
+  cloudinary_path TEXT NOT NULL,
+  taken_at timestamp with time zone default NOT NULL,
+  created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE videos (
+  id SERIAL PRIMARY KEY,
+  caption TEXT,
+  location TEXT,
+  aws_s3_path TEXT NOT NULL,
+  cloudinary_path TEXT NOT NULL,
+  taken_at timestamp with time zone default NOT NULL,
+  created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL
+);
---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+export default pool;
