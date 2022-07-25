33984e5ed3b65cabd94df7c8f77fc8e1df370b77
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-25 09:32:03 +0800

    refactor: update table names, and add is_hidden fields

--- src/models/database.sql
+++ src/models/database.sql
@@ -1,36 +1,39 @@
 CREATE DATABASE shots_ahmadihsan;
 
-CREATE TABLE photos (
+CREATE TABLE ig_photos (
   id SERIAL PRIMARY KEY,
   caption TEXT,
   location TEXT,
   path TEXT NOT NULL,
   aws_s3_path TEXT NOT NULL,
   cloudinary_path TEXT NOT NULL,
+  is_hidden BOOLEAN default FALSE,
   taken_at timestamp with time zone NOT NULL,
   last_modified_at timestamp with time zone NOT NULL,
   created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE stories (
+CREATE TABLE ig_stories (
   id SERIAL PRIMARY KEY,
   caption TEXT,
   location TEXT,
   path TEXT NOT NULL,
   aws_s3_path TEXT NOT NULL,
   cloudinary_path TEXT NOT NULL,
+  is_hidden BOOLEAN default FALSE,
   taken_at timestamp with time zone NOT NULL,
   last_modified_at timestamp with time zone NOT NULL,
   created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE videos (
+CREATE TABLE ig_videos (
   id SERIAL PRIMARY KEY,
   caption TEXT,
   location TEXT,
   path TEXT NOT NULL,
   aws_s3_path TEXT NOT NULL,
   cloudinary_path TEXT NOT NULL,
+  is_hidden BOOLEAN default FALSE,
   taken_at timestamp with time zone NOT NULL,
   last_modified_at timestamp with time zone NOT NULL,
   created_at timestamp with time zone default CURRENT_TIMESTAMP NOT NULL
