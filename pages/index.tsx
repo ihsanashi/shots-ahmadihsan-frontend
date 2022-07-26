55bba5455237e231c9b143ce0ed4684ade883877
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-26 10:00:02 +0800

    feat: replace temporary home page component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,43 @@
-import type { NextPage } from 'next'
+import Image from 'next/image';
+import { supabase } from '../utils/supabaseClient';
 
-const Home: NextPage = () => {
-  return (
-    <h1 className='text-3xl font-bold underline text-center'>Hello world!</h1>
-  )
+export async function getStaticProps({}) {
+  const { data, error } = await supabase
+    .from('ig_photos')
+    .select('*')
+    .range(0, 10);
+
+  return {
+    props: {
+      photos: data,
+    },
+  };
 }
 
-export default Home
+type Photo = {
+  id: number;
+  caption: string;
+  location: string;
+  aws_s3_path: string;
+  cloudinary_path: string;
+  taken_at: string;
+  created_at: string;
+};
+
+export default function Home({ photos }: { photos: Photo[] }) {
+  return (
+    <div>
+      {photos.map((photo) => (
+        <div key={photo.id}>
+          <Image
+            src={photo.cloudinary_path}
+            alt=''
+            height={400}
+            width={400}
+          />
+          <p>{photo.caption}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
