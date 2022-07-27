7f06e1ea3dd0bb82aa64e7c543c417741ec03e14
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-27 10:12:31 +0800

    chore: remove outdated components

--- src/components/FeedBar/FeedSwitcher.tsx
+++
@@ -1,9 +0,0 @@
-export const FeedSwitcher = ({}) => {
-  return (
-    <div className='flex justify-evenly'>
-      <button>Photos</button>
-      <button>Stories</button>
-      <button>Videos</button>
-    </div>
-  );
-};
