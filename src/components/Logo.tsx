55353206a831c607f9b0a2be185bbaff5a5e1941
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-30 07:52:32 +0800

    fix: add breakpoint font size for logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
     <div className='flex flex-row items-center'>
       <Link href='/'>
         <a>
-          <h3 className='text-2xl font-medium' aria-label='Wordmark logo, Ahmad Ihsan'>Ahmad Ihsan</h3>
+          <h3 className='text-lg font-medium md:text-2xl' aria-label='Wordmark logo, Ahmad Ihsan'>Ahmad Ihsan</h3>
         </a>
       </Link>
     </div>
