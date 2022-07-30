499dfae15b2a4a2652867eb3fa8627c299852a8c
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-30 20:01:37 +0800

    feat: implement dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import { ThemeProvider } from 'next-themes';
+import { darkTheme } from '../stitches.config';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// create react-query client
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        value={{
+          light: 'light',
+          dark: darkTheme.className,
+        }}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,24 +1,20 @@
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
-import { Button, Flex } from './';
-import { darkTheme } from '../../stitches.config';
-import { useState, useEffect } from 'react';
+import { Button, IconButton, Flex } from './';
+import { useTheme } from 'next-themes';
 import { Switch, SwitchThumb } from './Switch';
-import { IconButton } from './';
 
 export function ThemeSwitch() {
-  const [theme, setTheme] = useState('theme-default');
-
-  useEffect(() => {
-    document.body.classList.remove('theme-default', darkTheme);
-    document.body.classList.add(theme);
-  }, [theme]);
+  const { theme, setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
 
   return (
     <Flex justify='end' align='center'>
       <IconButton css={{ marginRight: '6px' }}>
         <SunIcon width={20} height={20} />
       </IconButton>
-      <Switch defaultChecked id='theme-switcher'>
+      <Switch defaultChecked id='theme-switcher' onClick={toggleTheme}>
         <SwitchThumb />
       </Switch>
       <IconButton css={{ marginLeft: '6px' }}>
