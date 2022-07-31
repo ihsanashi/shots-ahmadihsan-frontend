31e8fe56c1d9e82339690cd0cb01a77c8c6ab57c
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-31 08:58:13 +0800

    chore: hide ViewToolbar component in navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     <section className='max-w-full p-5 mx-auto lg:p-8 md:max-w-3xl lg:max-w-7xl'>
       <nav className='grid w-full h-12 grid-flow-col mx-auto auto-cols-auto'>
         <Logo />
-        <ViewToolbar />
+        {/* <ViewToolbar /> */}
         <ThemeSwitch />
       </nav>
     </section>
