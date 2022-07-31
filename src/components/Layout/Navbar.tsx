12f3fb7d319f661202acb479875f42c39a80f743
Ahmad Ihsan <ihsanashi@gmail.com>
2022-07-31 16:43:31 +0800

    feat: add temporary top menu

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,11 +1,22 @@
-import { Logo, ThemeSwitch, ViewToolbar } from '../index';
+import Link from 'next/link';
+import { Logo, ThemeSwitch } from '../index';
 
 export const Navbar = () => {
   return (
     <section className='max-w-full p-5 mx-auto lg:p-8 md:max-w-3xl lg:max-w-7xl'>
       <nav className='grid w-full h-12 grid-flow-col mx-auto auto-cols-auto'>
         <Logo />
-        {/* <ViewToolbar /> */}
+        <div className='flex items-center justify-between'>
+          <Link href='/gallery/photos'>
+            <a>Photos</a>
+          </Link>
+          <Link href='/gallery/stories'>
+            <a>Stories</a>
+          </Link>
+          <Link href='/gallery/videos'>
+            <a>Videos</a>
+          </Link>
+        </div>
         <ThemeSwitch />
       </nav>
     </section>
