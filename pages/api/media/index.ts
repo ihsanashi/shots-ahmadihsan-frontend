10e06f931ccba939a1552f0a39811572748e206e
Ahmad Ihsan <ihsanashi@gmail.com>
2022-08-28 14:29:29 +0800

    feat: migrate over using supabase to API route
    work in progress

--- pages/api/media/index.ts
+++ pages/api/media/index.ts
@@ -1,18 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '../../../src/utils/supabaseClient';
+import { supabase, getPagination } from '../../../src/utils';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (_req: NextApiRequest, res: NextApiResponse) => {
-  // res.status(200).json({ name: 'John Doe' });
-
-  const { data, error } = await supabase
+export default async (
+  _req: NextApiRequest,
+  res: NextApiResponse,
+  currentPage = 0,
+  perPage = 10
+) => {
+  const { from, to } = getPagination(currentPage, 10);
+  const { data, count, error } = await supabase
     .from('ig_backup')
-    .select()
-    .order('taken_at', { ascending: false });
+    .select('*', { count: 'exact' })
+    .order('taken_at', { ascending: false })
+    .range(from, to);
 
   if (error) {
     throw new Error(`${error.message}: ${error.details}`);
   }
 
-  res.status(200).json({ data });
+  res.status(200).json({
+    meta: {
+      perPage,
+      totalCount: count,
+      currentPage,
+      pageCount: 'page count',
+    },
+    data,
+  });
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,13 @@
 import Image from 'next/image';
-import { supabase } from '../src/utils/supabaseClient';
-import { HomepageData } from '../src/types';
 import { Container, Layout } from '../src/components';
 
-export async function getStaticProps() {
-  const { data, error } = await supabase
-    .from('ig_backup')
-    .select()
-    .order('taken_at', { ascending: false });
-
-  return {
-    props: {
-      data,
-    },
-  };
-}
-
-export default function Home({ data }: { data: HomepageData }) {
-  const HOMEPAGE_MEDIA = Object.values(data).flat();
+export default function Home() {
 
   return (
     <Layout>
       <Container>
         <div className='grid grid-cols-2 gap-1 md:grid-cols-3'>
-          {HOMEPAGE_MEDIA.map((media) => (
-            <div key={media.id}>
-              {media.path.toLocaleLowerCase().includes('mp4') ? (
-                <video height={400} width={400} controls>
-                  <source src={media.cloudinary_path} type='video/mp4' />
-                  <source src={media.aws_s3_path} type='video/mp4' />
-                  Your browser does not support the video tag.
-                </video>
-              ) : (
-                <Image
-                  src={media.cloudinary_path}
-                  alt=''
-                  height={400}
-                  width={400}
-                />
-              )}
-            </div>
-          ))}
+          <h1>Hello world</h1>
         </div>
       </Container>
     </Layout>
