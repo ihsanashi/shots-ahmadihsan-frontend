a08a2c9d397b01e7c438deb69a036eb444ca45e6
Ahmad Ihsan <ihsanashi@gmail.com>
2022-08-28 14:27:11 +0800

    chore: add swr, remove react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-
 import { ThemeProvider } from 'next-themes';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-// create react-query client
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute='class' defaultTheme='system'>
-        <Component {...pageProps} />
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeProvider attribute='class' defaultTheme='system'>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
