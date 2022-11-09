1d7e46dc0afade9ff17f5103b74d8e04ad890f98
Ahmad Ihsan <ihsanashi@gmail.com>
2022-11-09 15:57:56 +0800

    feat: fetch data from graphql API with apollo
    Populate homepage with json stringify. Still needs a lot of work on the layout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import Image from 'next/image';
+import { useQuery, gql } from '@apollo/client';
+import { client, HOME_DATA } from '../lib/apollo';
 import { Container, Layout } from '../src/components';
+import { Asset } from '../src/interfaces';
 
-export default function Home() {
+interface AssetProps {
+  assets: Asset[];
+}
 
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: HOME_DATA,
+  });
+
+  return {
+    props: {
+      assets: data.allAsset,
+    },
+  };
+}
+
+export default function Home({ assets }: AssetProps) {
+  console.log('assets: ', assets);
   return (
     <Layout>
       <Container>
         <div className='grid grid-cols-2 gap-1 md:grid-cols-3'>
-          <h1>Hello world</h1>
+          {JSON.stringify(assets)}
         </div>
       </Container>
     </Layout>
