520bb781de7e2c04febf18626fd56e4cd6259862
Ahmad Ihsan <ihsanashi@gmail.com>
2022-11-09 15:56:44 +0800

    feat: add asset interface

---
+++ src/interfaces/asset.ts
@@ -0,0 +1,11 @@
+export type Asset = {
+  _id: number;
+  caption: string;
+  location: string;
+  visible: boolean;
+  path: string;
+  aws_s3_path: string;
+  cloudinary_path: string;
+  taken_at: string;
+  created_at: string;
+};
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,2 @@
 export type { Params } from './params';
+export type { Asset } from './asset';
