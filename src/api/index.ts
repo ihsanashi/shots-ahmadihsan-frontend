719e7ec647ba143bb2bc97e252019f9002eb0507
Ahmad Ihsan <ihsanashi@gmail.com>
2022-11-09 11:55:18 +0800

    chore: remove anything supabase related

--- src/api/index.ts
+++
@@ -1,3 +0,0 @@
-export { getPhotos, getSinglePhoto } from './photos';
-export { getStories, getSingleStory } from './stories';
-export { getVideos, getSingleVideo } from './videos';
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { supabase } from './supabaseClient';
-export { getPagination } from './pagination';
-export { fetcher } from './fetcher';
