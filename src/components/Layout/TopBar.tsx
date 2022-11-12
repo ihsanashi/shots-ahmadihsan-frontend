0ac727be078ea89a1a619b079b02747980876fb5
Ahmad Ihsan <ihsanashi@gmail.com>
2022-11-12 17:10:16 +0800

    chore: upgrade next.js, remove a tag in Link component

--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -4,9 +4,7 @@ export const TopBar = () => {
   return (
     <section className='max-w-full p-4'>
       <Link href='/'>
-        <a>
-          <h3 className='text-2xl md:text-4xl'>@ahmadihsan</h3>
-        </a>
+        <h3 className='text-2xl md:text-4xl'>chanmalichann</h3>
       </Link>
     </section>
   );
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -4,14 +4,12 @@ export const Logo = () => {
   return (
     <div className='flex flex-row items-center'>
       <Link href='/'>
-        <a>
-          <h3
-            className='text-lg font-normal uppercase md:text-2xl'
-            aria-label='Wordmark logo, Ahmad Ihsan'
-          >
-            Ahmad Ihsan
-          </h3>
-        </a>
+        <h3
+          className='text-lg font-normal uppercase md:text-2xl'
+          aria-label='Wordmark logo, Ahmad Ihsan'
+        >
+          Ahmad Ihsan
+        </h3>
       </Link>
     </div>
   );
